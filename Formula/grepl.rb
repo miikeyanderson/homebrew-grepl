3b6ee1d29a2f0190bf1c94172dca7b9351b0a43e
Michael <m.anderson23@icloud.com>
2026-01-09 17:02:45 -0600

    Update grepl to 0.2.3

--- Formula/grepl.rb
+++ Formula/grepl.rb
@@ -3,8 +3,8 @@ class Grepl < Formula
 
   desc "Semantic code search CLI using ChromaDB + Ollama"
   homepage "https://github.com/miikeyanderson/grepl"
-  url "https://github.com/miikeyanderson/grepl/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "a5adcd0b88c9ded817e2016069d405c0c2a04e3264f652ff4cf56810cf63859c"
+  url "https://github.com/miikeyanderson/grepl/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "ec47803842bdee09c600a9ff3c434899aa0af668a01f6c0bd44979fd207cf13d"
   license "MIT"
 
   depends_on "python@3.11"
